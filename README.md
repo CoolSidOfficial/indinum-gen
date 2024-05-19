712f169385d8a26fb8b9525c5f2197883a02c4f0
CoolSidOfficial <hackersiddhant1@gmail.com>
2024-05-19 11:10:14 +0530

    intialization

---
+++ README.md
@@ -0,0 +1,1 @@
+# indian phone numbers generator
---
+++ num_gen.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<string>
+std::vector<std::string> indianStates = {
+	    "Andhra Pradesh",
+	        "Arunachal Pradesh",
+		    "Assam",
+		        "Bihar",
+			    "Chhattisgarh",
+			        "Goa",
+				    "Gujarat",
+				        "Haryana",
+					    "Himachal Pradesh",
+					        "Jharkhand",
+						    "Karnataka",
+						        "Kerala",
+							    "Madhya Pradesh",
+							        "Maharashtra",
+								    "Manipur",
+								        "Meghalaya",
+									    "Mizoram",
+									        "Nagaland",
+										    "Odisha",
+										        "Punjab",
+											    "Rajasthan",
+											        "Sikkim",
+												    "Tamil Nadu",
+												        "Telangana",
+													    "Tripura",
+													        "Uttar Pradesh",
+														    "Uttarakhand",
+														        "West Bengal"
+};
+void intro_func()
+{
+
+
+
+
+	cout<<"Please choose your state\n";
+	for
+
+}
+int main(){
+
+
+	
+return 0;
+}
